2e68f32dff65fa7803fccea5e01271367039effd
Dominic Fleckner <admin@markd-up.info.tm>
2013-04-09 19:34:36 +0200

    Rearranged file-structure.

--- comps/loginscript.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	require_once '../classes/users.php';
-	$users = new users();
-	if($_POST && !empty($_POST['name']) && !empty($_POST['pass'])){
-		$users->validate_user($_POST['name'], $_POST['pass']);
-	}
-?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-require_once 'classes/users.php';
-$users = new users();
-$users->logout();
-?>
